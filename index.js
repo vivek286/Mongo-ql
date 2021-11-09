e022aa7c3e38e0135137231903b43cc04524db6c
vivek286 <solankivivek01@gmail.com>
2021-11-09 20:03:31 +0530

    fetching keys

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const keyify = (obj, prefix = '') => 
+  Object.keys(obj).reduce((res, el) => {
+    if( Array.isArray(obj[el]) ) {
+      return res;
+    } else if( typeof obj[el] === 'object' && obj[el] !== null ) {
+      return [...res, ...keyify(obj[el], prefix + el + '.')];
+    }
+    return [...res, prefix + el];
+  }, []);
 async function mongo_crud(options,actions){
     const {MongoClient,ObjectId} = require('mongodb');
     var uri="";
@@ -44,8 +53,33 @@ async function mongo_crud(options,actions){
     }
     var result={};
         if(actions!=null){
-        //To transform ql filter key to mongodb filter key
-            return result;
+    //     var keys = Object.keys(actions);
+    //     for(var i=0;i<keys.length;i++){
+    //         var obj;
+    //         if(actions[keys[i]]==""){
+    //    obj=await sheet[keys[i]]().toArray();}
+    //    else{
+    //     obj=await sheet[keys[i]](actions[keys[i]]);
+    //    }
+    //   result[keys[i]]=obj;
+    //     }
+    //     // console.log(keys);
+    //     // console.log(result);
+    //         return result;
+            var temp1=actions.filter;
+            var temp2=keyify(temp1);
+            var keys=Object.keys(temp1);
+            for(var i=0;i<keys.length;i++){
+                temp2[$+`${keys[i]}`]=temp1[keys[i]];
+            }
+            for(var i=0;i<keys.length;i++){
+                var temp3=temp1.and;
+                var temp4=temp3;
+                while(temp3!=null){
+
+
+                }
+            }
         }
 }
 
